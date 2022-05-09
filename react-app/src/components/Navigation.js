04006dd58b4474c4e89dd5bf6a5cd9cc58781d1d
Samad-sust <masamad.sust@gmail.com>
2022-05-09 08:29:41 +0600

    design improvements

--- react-app/src/components/Navigation.js
+++ react-app/src/components/Navigation.js
@@ -23,7 +23,7 @@ function Navbar() {
 
                         <nav className="main-menu">
                             <ul>
-                                <li className="text-dark"><Link to="/">Home</Link></li>
+                                <li><Link to="/">Home</Link></li>
                                 <li><Link to="/about" >About</Link></li>
                                 <li><Link to="/articles" >Articles</Link></li>
                                 <li>
